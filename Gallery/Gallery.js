2796167200f29f606e30ed0263dac022a8b6ca0d
Orthalange Tharushi Madubhashinie <159515567+Madubhashinie@users.noreply.github.com>
2025-02-17 11:59:21 +0530

    Created simple Gallery using HTML CSS JS

---
+++ Gallery/Gallery.js
@@ -0,0 +1,100 @@
+function showFoodSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("foodSection").style.display = "block";
+}
+function showEducationSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("educationSection").style.display = "block";
+}
+function showNatureSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("natureSection").style.display = "block";
+}
+function showTravelSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("travelSection").style.display = "block";
+}
+function showTechnologySection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("technologySection").style.display = "block";
+}
+function showArtSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("artSection").style.display = "block";
+}
+function showSportSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("sportSection").style.display = "block";
+}
+function showfashionSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("fashionSection").style.display = "block";
+}
+function showArchitectureSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("architectureSection").style.display = "block";
+}
+function showMusicSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("musicSection").style.display = "block";
+}
+function showSpaceSection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("spaceSection").style.display = "block";
+}
+function showHistorySection() {
+    // Hide other sections
+    document.getElementById("mainGallery").style.display = "none";
+    document.getElementById("h1").style.display = "none";
+    // Show the food section
+    document.getElementById("historySection").style.display = "block";
+}
+function goBack() {
+    document.getElementById("foodSection").style.display = "none";
+    document.getElementById("educationSection").style.display = "none";
+    document.getElementById("natureSection").style.display = "none";
+    document.getElementById("travelSection").style.display = "none";
+    document.getElementById("technologySection").style.display = "none";
+    document.getElementById("artSection").style.display = "none";
+    document.getElementById("sportSection").style.display = "none";
+    document.getElementById("fashionSection").style.display = "none";
+    document.getElementById("architectureSection").style.display = "none";
+    document.getElementById("musicSection").style.display = "none";
+    document.getElementById("spaceSection").style.display = "none";
+    document.getElementById("historySection").style.display = "none";
+    document.getElementById("mainGallery").style.display = "block";
+    document.getElementById("h1").style.display = "block";
+}
